396f569a75a505913bc099575d26c29a3274fe9b
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-13 15:35:46 +0100

    feat: Add more messages and .gitignore

--- display.py
+++ display.py
@@ -1,5 +1,25 @@
+# Inputs
+def input_operation():
+    return input("Please type your operation:\n")
+
+# Messages
 def message_welcome():
     print("Welcome to MyCalculator®!")
 
-def input_operation():
-    input("Please type your operation:\n")
+# Errors
+def error_format():
+    print("Invalid format!")
+
+def error_invalid_chara():
+    print("Invalid characted detected!")
+
+def error_division_zero():
+    print("Can't divide by 0! (would make +∞, -∞ or 0 anyway)")
+
+# History
+def history(value): #TODO temp variable; Need to know what History returns
+    print(value)
+
+# Result
+def result(value): #TODO temp variable; Need to know how to print result
+    print(f"= {value}")
--- main.py
+++ main.py
@@ -2,24 +2,32 @@ import display
 
 def user_input():   
     while True:
+        invalid = 0
+
         try:
-            operation_field = display.ask_operation()
+            operation_field = display.input_operation()
         except ValueError:
-            print("Invalid format")
+            display.error_format()
+            continue
+
         valid_characters = "0123546789+-*/ "
         for i in range(len(operation_field)):
             if operation_field[i] not in valid_characters:
-                print("No.")
+                display.error_invalid_chara()
+                invalid = 1
                 break 
             else:
                 continue
+        if invalid == 1:
+            invalid = 0
+            continue
         
         numbers_temp, operators_temp = operation_field, operation_field
         for symbol in valid_characters[10:]:
             numbers_temp = numbers_temp.replace(symbol, " ")
         numbers = numbers_temp.split()
         for digit in valid_characters[:10]:
-            operators_temp = operators_temp.replace(digit, " ") #.strip()
+            operators_temp = operators_temp.replace(digit, " ")
         operators = operators_temp.split()
 
         result = float(numbers[0])
@@ -32,12 +40,12 @@ def user_input():
                     try:
                         result = result / float(numbers[i+1])
                     except ZeroDivisionError:
-                        print("Ha!")
+                        display.error_division_zero()
 
         return result
 def main():
     while True:
         final_result = user_input()
-        print(final_result)
+        display.result(final_result)
 
 main()
