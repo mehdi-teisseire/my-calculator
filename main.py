99050ef2afc17d5b4e5d483dc918615ccbbd725f
Mehdi <mehdi.teisseire@laplateforme.io>
2025-01-13 11:13:29 +0100

    add: Basic calculator

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-def user_input():    
+def add(x,y):
+    return y + x
 
-    try:
-        number_1 = float(input("Your first number"))
-        number_2 = float(input("Your second number"))
-    except ValueError:
-        print("Invalid format")
+def sub(x,y):
+    return x - y
 
-user_input()
+def multi(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+print("Welcome to my calculator")
+print("1 for add")
+print("2 for sub")
+print("3 for multi")
+print("4 for divide")
+while True:
+    choice_input=input("Enter your choice")
+    if choice_input in ("1","2","3","4"):
+
+        try:
+            num_1 = float(input("Enter first number"))
+            num_2 = float(input("Enter second number"))
+        except ValueError:
+            print("wrong format")
+            continue
+        
+        if choice_input == "1":
+            print (num_1,"+",num_2,"=",add(num_1,num_2))
+        elif choice_input =="2":
+            print (num_1,"-",num_2,"=",sub(num_1,num_2))
+        elif choice_input == "3":
+            print (num_1,"x",num_2,"=",multi(num_1,num_2))
+        elif choice_input =="4":
+            print (num_1,"/",num_2,"=",divide(num_1,num_2))
+        
+
+
+    
