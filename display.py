15dcb561df6995a2f4485d523c772ef4802b1fc7
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-14 13:00:00 +0100

    Feat: Added more error message

---
+++ display.py
@@ -0,0 +1,32 @@
+# Inputs
+def input_operation():
+    return input("Please type your operation:\n")
+
+# Messages
+def message_welcome():
+    print("Welcome to MyCalculator®!")
+
+# Errors
+def error_format():
+    print("Invalid format!")
+
+def error_invalid_chara():
+    print("Invalid characted detected!")
+
+def error_division_zero():
+    print("Can't divide by 0! (would make +∞, -∞ or 0 anyway)")
+
+def error_not_openable():
+    print("Something went wrong when opening the file!")
+
+def error_not_writable():
+    print("Something went wrong when writing the file!")
+
+
+# History
+def history(value): #TODO temp variable; Need to know what History returns
+    print(value)
+
+# Result
+def result(value): #TODO temp variable; Need to know how to print result
+    print(f"= {value}")
