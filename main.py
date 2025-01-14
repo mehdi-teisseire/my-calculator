3c50e5e1b1d3a60ef55d0c89951dd47ce8c646e0
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-14 15:38:22 +0100

    refactor: Move User_input in rules.py and add import

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
-def user_input():   
-    while True:
-        try:
-            operation_field = input("Calculate something:\n")
-        except ValueError:
-            print("Invalid format")
-        valid_characters = "0123546789+-*/ "
-        for i in range(len(operation_field)):
-            if operation_field[i] not in valid_characters:
-                print("No.")
-                break 
-            else:
-                continue
-        
-        numbers_temp, operators_temp = operation_field, operation_field
-        for symbol in valid_characters[10:]:
-            numbers_temp = numbers_temp.replace(symbol, " ")
-        numbers = numbers_temp.split()
-        for digit in valid_characters[:10]:
-            operators_temp = operators_temp.replace(digit, " ") #.strip()
-        operators = operators_temp.split()
+import history, rules, display
 
-        result = float(numbers[0])
-        for i in range(len(operators)):
-            match operators[i]:
-                case "+": result = result + float(numbers[i+1])
-                case "-": result = result - float(numbers[i+1])
-                case "*": result = result * float(numbers[i+1])
-                case "/": 
-                    try:
-                        result = result / float(numbers[i+1])
-                    except ZeroDivisionError:
-                        print("Ha!")
-
-        return result
 def main():
     while True:
-        final_result = user_input()
-        print(final_result)
+        #while True
+            #graphic_type = display.input_graphics_type()
+        #if graphic_type = True:
+        #else:
+            user_str = display.input_operation()
+            #history.open()
+            final_result = resolve_math_input(user_str)
+            display.final_result(final_result)
 
 main()
