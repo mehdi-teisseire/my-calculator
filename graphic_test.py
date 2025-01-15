5a6c0328a05a6aea8c17ab38ef6f4e7e80d1ef83
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-15 18:11:16 +0100

    fix: Fix broken buttons display

--- graphic_test.py
+++ graphic_test.py
@@ -4,12 +4,27 @@ class OperatorFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        self.button_plus = customtkinter.CTkButton(self, height= 10, width= 10, text="+", command=self.button_input_character)
+        self.button_plus.grid(row = 0, column = 0)
 
+        self.button_minus = customtkinter.CTkButton(self, text="-", command=self.button_input_character)
+        self.button_minus.grid(row = 0, column = 1)
+
+    def button_input_character(self):
+        print("ok")
 
 class NumbersFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        self.button_plus = customtkinter.CTkButton(self, text="+", command=self.button_input_character)
+        self.button_plus.grid(row = 0, column = 0)
+
+        self.button_minus = customtkinter.CTkButton(self, text="-", command=self.button_input_character)
+        self.button_minus.grid(row = 0, column = 1)
+
+    def button_input_character(self):
+        print("ok")
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -21,13 +36,17 @@ class App(customtkinter.CTk):
         self.operator_frame.grid(row = 1, column = 4, columnspan = 3, rowspan = 3, sticky="nes")
 
         self.numbers_frame = NumbersFrame(self)
-        self.numbers_frame.grid(row = 1, column = 0, rowspan = 3, sticky="nws")
+        self.numbers_frame.grid(row = 1, column = 0, columnspan = 3, rowspan = 3, sticky="nws")
 
         self.columnconfigure(0, weight = 1)
         self.columnconfigure(1, weight = 1)
         self.columnconfigure(2, weight = 1)
+
         self.columnconfigure(3, weight = 1)
+        
         self.columnconfigure(4, weight = 1)
+        self.columnconfigure(5, weight = 1)
+        self.columnconfigure(6, weight = 1)
         
         self.rowconfigure(0, weight = 3)
         self.rowconfigure(1, weight = 1)
--- graphics.py
+++ graphics.py
@@ -1,24 +1,75 @@
 import customtkinter
 
+class InputFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.rowconfigure(0)
+        self.columnconfigure(0)
+
+        # input that accept direct keys or buttons
+        # display result in input directly or in label next to it (weight 4:1)
+
+        
+
+
+class NumbersFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        for i in range(3): 
+            self.columnconfigure(i ,weight = 1)
+
+        for i in range(4):
+            self.rowconfigure(i, weight = 1)
+
+        self.button_numbers = []
+
+        #Numbers buttons #i%3 = 0 1 2 0 1 2 0 1 2      #i//3 = 0 0 0 1 1 1 2 2 2
+        self.button_numbers.append(customtkinter.CTkButton(self, text="0", command=self.button_input_character))
+        self.button_numbers[0].grid(row = 3, column = 0, columnspan = 2, padx = 2, pady = 2, sticky = "news") 
+
+        for i in range(1, 10):
+            self.button_numbers.append(customtkinter.CTkButton(self, text=i, command=self.button_input_character))
+            self.value = i
+            self.button_numbers[i].grid(row = (i-1)//3, column = (i-1)%3, padx = 2, pady = 2, sticky = "news")
+
+        self.button_numbers.append(customtkinter.CTkButton(self, text=".", command=self.button_input_character))
+        self.button_numbers[10].grid(row = 3, column = 2, padx = 2, pady = 2, sticky = "news") 
+
+    def button_input_character(self):
+        print(self.value)
+        #test = self.cget("text")
+        #print(test)
+
 class OperatorFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        #Operator buttons
-        self.button_equal = customtkinter.CTkButton(self, text="=", command=self.button_display_result)
-        self.button_equal.grid(row = 3, column = 4, rowspan = 2, columnspan = 2)
+        # Columns in frame Operator
+        for i in range(2): 
+            self.columnconfigure(i ,weight = 1)
+            
+        # Rows in frame Operator
+        for i in range(2):
+            self.rowconfigure(i, weight = 1)
+        self.rowconfigure(2, weight = 2)
 
+        # Operator buttons
         self.button_plus = customtkinter.CTkButton(self, text="+", command=self.button_input_character)
-        self.button_plus.grid(row = 1, column = 4)
+        self.button_plus.grid(row = 0, column = 0, padx= 2, pady = 2, sticky = "news")
 
         self.button_minus = customtkinter.CTkButton(self, text="-", command=self.button_input_character)
-        self.button_minus.grid(row = 1, column = 5)        
+        self.button_minus.grid(row = 0, column = 1, padx= 2, pady = 2, sticky = "news")
 
         self.button_multiply = customtkinter.CTkButton(self, text="*", command=self.button_input_character)
-        self.button_multiply.grid(row = 2, column = 4)
+        self.button_multiply.grid(row = 1, column = 0, padx= 2, pady = 2, sticky = "news")
 
         self.button_divide = customtkinter.CTkButton(self, text="/", command=self.button_input_character)
-        self.button_divide.grid(row = 2, column = 5)
+        self.button_divide.grid(row = 1, column = 1, padx= 2, pady = 2, sticky = "news")
+        
+        self.button_equal = customtkinter.CTkButton(self, text="=", command=self.button_display_result)
+        self.button_equal.grid(row = 2, column = 0, columnspan = 2, padx = 2, pady = 20, sticky = "news")
 
     def button_input_character(self):
         print(self.value)
@@ -28,73 +79,34 @@ class OperatorFrame(customtkinter.CTkFrame):
     def button_display_result(self):
         print(self)
 
-class NumbersFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.button_numbers = []
-
-        #Numbers buttons #i%3 = 0 1 2 0 1 2 0 1 2      #i//3 = 0 0 0 1 1 1 2 2 2
-        self.button_numbers.append(customtkinter.CTkButton(self, text="0", command=self.button_input_character))
-        self.button_numbers[0].grid(row = 4, column = 0, columnspan = 3) 
-        for i in range(1, 10):
-            self.button_numbers.append(customtkinter.CTkButton(self, text=i, command=self.button_input_character))
-            self.value = i
-            self.button_numbers[i].grid(row = (i-1)//3, column = (i-1)%3)
-
-    def button_input_character(self):
-        print(self.value)
-        #test = self.cget("text")
-        #print(test)
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("MyCaLcuLatoR")
-        self.geometry("400x600")
+        self.geometry("500x600")
 
-        #Configure row/column
-        self.columnconfigure(0, weight = 1)
-        for i in range(3): self.columnconfigure(1 ,weight = 1)
-        self.columnconfigure(3, weight= 2)
-        for i in range(4,6): self.columnconfigure(i, weight=1)
+        # Configure row/column
+        self.columnconfigure(0, weight= 2)
+        self.columnconfigure(1, weight= 2)
 
         self.rowconfigure(0, weight = 1)
-        for i in range(1,5): self.rowconfigure(1, weight = 2)
-        
+        self.rowconfigure(1, weight = 2)
 
-        #self.result_frame = ResultFrame(self)
-        #self.result_frame.grid(row = 0, column = 0, sticky="nsew")
-
-        self.operator_frame = OperatorFrame(self)
-        self.operator_frame.grid(row = 1, column = 4, sticky="ns")
+        # Frames
+        self.input_frame = InputFrame(self)
+        self.operator_frame.grid(row = 0, column = 0, columnspan= 2, sticky="ns")
 
         self.numbers_frame = NumbersFrame(self)
-        self.numbers_frame.grid(row = 1, column = 0, sticky="ns")
+        self.numbers_frame.grid(row = 1, column = 0, sticky="news")
+
+            #maybe a separator here?
+
+        self.operator_frame = OperatorFrame(self)
+        self.operator_frame.grid(row = 1, column = 1, sticky="news")
+
+            #history on a new window
+        #self.history_frame = HistoryFrame(self)
+        #self.operator_frame.grid
 
 app = App()
 app.mainloop()
-
-
-    #   self.button_equal = customtkinter.CTkButton(self, text="=", command=self.button_display_result)
-    #     self.button_equal.grid(row= 2, column = 4, rowspan = 2, columnspan = 2)
-
-    #     self.button_plus = customtkinter.CTkButton(self, text="+", command=self.button_input_character)
-    #     self.button_plus.grid(row= 1, column = 4)
-
-    #     self.button_minus = customtkinter.CTkButton(self, text="-", command=self.button_input_character)
-    #     self.button_minus.grid(row= 1, column = 5)        
-
-    #     self.button_multiply = customtkinter.CTkButton(self, text="*", command=self.button_input_character)
-    #     self.button_multiply.grid(row= 2, column = 4)
-
-    #     self.button_divide = customtkinter.CTkButton(self, text="/", command=self.button_input_character)
-    #     self.button_divide.grid(row= 2, column = 5)
-
-    #     self.button_numbers = []
-
-    #     #Numbers buttons #i%3 = 0 1 2 0 1 2 0 1 2      #i//3 = 0 0 0 1 1 1 2 2 2
-    #     self.button_numbers.append(customtkinter.CTkButton(self, text="0", command=self.button_input_character))
-    #     self.button_numbers[0].grid(row = 4, column = 1, columnspan = 3, sticky="ew") 
-    #     for i in range(1, 10):
-    #         self.button_numbers.append(customtkinter.CTkButton(self, text=i, command=self.button_input_character))
-    #         self.button_numbers[i].grid(row = (i//3)+1, column = i%3, sticky="ew") 
