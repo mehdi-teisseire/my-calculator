4f861a55b326a45a99245d8324263b5787d4d6a0
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-17 16:26:13 +0100

    refractor: Make loops works for buttons

--- graphics.py
+++ graphics.py
@@ -1,4 +1,4 @@
-#import rules
+import rules, history
 import customtkinter
 
 class InputFrame(customtkinter.CTkFrame):
@@ -33,45 +33,27 @@ class NumbersFrame(customtkinter.CTkFrame):
         self.button_memory = customtkinter.CTkButton(self, text="DEL", font=master.fonts, command=lambda: master.input_operation.delete(int(len(master.input_operation.get())-1), 'end')) #replace with last value
         self.button_memory.grid(row = 0, column = 2, padx = 2, pady = 2, sticky = "news") 
 
-        self.button_numbers.append(customtkinter.CTkButton(self, text="0", font=master.fonts, command=lambda: master.input_operation.insert('end', "0")))
-        self.button_numbers[0].grid(row = 4, column = 0, columnspan = 2, padx = 2, pady = 2, sticky = "news") 
+        self.button_numbers.append(customtkinter.CTkButton(self, text="0", font=master.fonts, command=lambda: self.button_input_character(master, "0")))
+        self.button_numbers[0].grid(row=4, column=0, columnspan=2, padx=2, pady=2, sticky="news")
 
-        self.button_numbers.append(customtkinter.CTkButton(self, text="1", font=master.fonts, command=lambda: master.input_operation.insert('end', "1")))
-        self.button_numbers[1].grid(row = 1, column = 0, padx = 2, pady = 2, sticky = "news") 
+        for i in range(1, 10):
+            self.button_numbers.append(customtkinter.CTkButton(self, text=i, font=master.fonts, command=lambda i=i: self.button_input_character(master, str(i))))
+            self.button_numbers[i].grid(row=((i-1)//3)+1, column=(i-1)%3, padx=2, pady=2, sticky="news")
 
-        self.button_numbers.append(customtkinter.CTkButton(self, text="2", font=master.fonts, command=lambda: master.input_operation.insert('end', "2")))
-        self.button_numbers[2].grid(row = 1, column = 1, padx = 2, pady = 2, sticky = "news") 
+        self.button_numbers.append(customtkinter.CTkButton(self, text=".", font=master.fonts, command=lambda: self.button_input_character(".")))
+        self.button_numbers[10].grid(row=4, column=2, padx=2, pady=2, sticky="news")
 
-        self.button_numbers.append(customtkinter.CTkButton(self, text="3", font=master.fonts, command=lambda: master.input_operation.insert('end', "3")))
-        self.button_numbers[3].grid(row = 1, column = 2, padx = 2, pady = 2, sticky = "news") 
+    def button_input_character(self, master, text):
+        if master.flag_clear == True:
+            master.input_operation.delete(0, len(master.input_operation.get()))
+            master.flag_clear = False
 
-        self.button_numbers.append(customtkinter.CTkButton(self, text="4", font=master.fonts, command=lambda: master.input_operation.insert('end', "4")))
-        self.button_numbers[4].grid(row = 2, column = 0, padx = 2, pady = 2, sticky = "news") 
+        current_text = self.master.input_operation.get()
+        new_text = current_text + text
+        self.master.input_operation.delete(0, customtkinter.END) 
+        self.master.input_operation.insert(0, new_text) 
 
-        self.button_numbers.append(customtkinter.CTkButton(self, text="5", font=master.fonts, command=lambda: master.input_operation.insert('end', "5")))
-        self.button_numbers[5].grid(row = 2, column = 1, padx = 2, pady = 2, sticky = "news") 
-
-        self.button_numbers.append(customtkinter.CTkButton(self, text="6", font=master.fonts, command=lambda: master.input_operation.insert('end', "6")))
-        self.button_numbers[6].grid(row = 2, column = 2, padx = 2, pady = 2, sticky = "news") 
-
-        self.button_numbers.append(customtkinter.CTkButton(self, text="7", font=master.fonts, command=lambda: master.input_operation.insert('end', "7")))
-        self.button_numbers[7].grid(row = 3, column = 0, padx = 2, pady = 2, sticky = "news") 
-
-        self.button_numbers.append(customtkinter.CTkButton(self, text="8", font=master.fonts, command=lambda: master.input_operation.insert('end', "8")))
-        self.button_numbers[8].grid(row = 3, column = 1, padx = 2, pady = 2, sticky = "news") 
-
-        self.button_numbers.append(customtkinter.CTkButton(self, text="9", font=master.fonts, command=lambda: master.input_operation.insert('end', "9")))
-        self.button_numbers[9].grid(row = 3, column = 2, padx = 2, pady = 2, sticky = "news") 
-
-        self.button_dot = customtkinter.CTkButton(self, text=".", font=master.fonts, command=lambda: master.input_operation.insert('end', "."))
-        self.button_dot.grid(row = 4, column = 2, padx = 2, pady = 2, sticky = "news") 
-
-    def button_input_character(self):
-        pass
-        # result = rules.user_input(.input_operation.get())
-        #test = self.cget("text")
-        #print(test)
-
+        
 class OperatorFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
@@ -121,12 +103,27 @@ class OperatorFrame(customtkinter.CTkFrame):
             final_result = rules.resolve_math_input(result)  
             master.input_operation.delete(0, customtkinter.END) 
             master.input_operation.insert(0, final_result) 
+            self.print_file("history.txt")
+            master.flag_clear = True
 
     def handle_terminal_input(self):
         operation_input = rules.user_input_terminal() 
         if operation_input:
             final_result = rules.resolve_math_input(operation_input)
             print(f"Résultat : {final_result}")
+
+    def print_file(filename, master):
+            # if not filename.endswith('.txt'):
+            #     filename += '.txt'
+            try:
+                with open(filename,'r') as file :
+                    content = file.read()
+                    master.history_window.text_file.delete(0,'end')
+                    master.history_window.text_file.insert(0, history(content))
+                    print(content)
+                    #self.configfile.insert(0, history(content))
+            except Exception as e:
+                pass
 
 
 class HistoryWindow(customtkinter.CTkToplevel):
@@ -135,7 +132,11 @@ class HistoryWindow(customtkinter.CTkToplevel):
         self.title("History")
         self.geometry("300x600")
 
-        print('selected:', history.txt )
+        self.columnconfigure(0, weight = 1)
+        self.rowconfigure(0, weight = 1)
+
+        text_file = customtkinter.CTkTextbox(self, state = "disabled")
+        text_file.grid(row = 0, column = 0, sticky = "news")
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -152,6 +153,8 @@ class App(customtkinter.CTk):
         self.rowconfigure(1, weight = 2)
 
         self.fonts = customtkinter.CTkFont(family = 'Arial', size = 30)
+
+        self.flag_clear = False
 
         # Input 
         self.input_operation = customtkinter.CTkEntry(self, font=self.fonts, placeholder_text="type something")
